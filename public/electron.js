23e3824d694cc15546606950bb49c17b510583af
H4ck3r-x0 <h4ck3r.x0@gmail.com>
2020-01-25 14:13:30 +0300

    add helper function to determin links on the text, and fix opining the link on the default browser.

--- public/electron.js
+++ public/electron.js
@@ -23,6 +23,10 @@ function createWindow() {
     mainWindow.webContents.openDevTools();
   }
   mainWindow.on("closed", () => (mainWindow = null));
+  mainWindow.webContents.on("new-window", function(e, url) {
+    e.preventDefault();
+    require("electron").shell.openExternal(url);
+  });
 }
 
 app.on("ready", createWindow);
--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import moment from "moment";
 import firebase from "../firebase";
 import Loader from "react-loader-spinner";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
@@ -37,7 +38,8 @@ class App extends Component {
 		const db = firebase.firestore();
 
 		const userRef = db.collection("urls").add({
-			url: this.state.url
+			url: this.state.url,
+			created_at: new Date().getTime()
 		});
 		this.setState({
 			url: ""
@@ -52,12 +54,22 @@ class App extends Component {
 							key={key}
 							className="flex flex-col text-sm bg-gray-200 px-4 py-1 mb-3 border-l-2 border-red-400 rounded-lg shadow-md "
 						>
-							<span className="font-bold text-red-400">
-								Someone just shared a link
+							<span className="flex flex-row justify-between items-center font-bold text-red-400">
+								<span> Someone just shared a link</span>
+								<span className="flex text-xs font-bold text-gray-500 ">
+									{moment(item.created_at).format(
+										"YYYY-MM-DD, hh:mm:ss A"
+									)}
+								</span>
 							</span>
 							<span className="text-xs text-blue-600 pl-3 mt-1">
 								<i className="fas fa-hand-point-right text-gray-700 mr-2"></i>
-								{item.url}
+
+								<p
+									dangerouslySetInnerHTML={replaceURLWithHTMLLinks(
+										item.url
+									)}
+								></p>
 							</span>
 						</li>
 					))}
@@ -85,8 +97,8 @@ class App extends Component {
 						<form onSubmit={this.shareURL}>
 							<input
 								name="url"
-								type="url"
-								className="flex w-full px-4 py-3 border-b-2 border-gray-300 focus:outline-none"
+								type="text"
+								className="flex w-full px-4 py-3 border-b-3 border-gray-300 focus:outline-none shadow"
 								placeholder="Share a url with your friends"
 								onChange={this.updateInput}
 								value={this.state.url}
@@ -98,6 +110,11 @@ class App extends Component {
 			</div>
 		);
 	}
+}
+
+function replaceURLWithHTMLLinks(text) {
+	var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
+	return { __html: text.replace(exp, "<a target='blank' href='$1'>$1</a>") };
 }
 
 export default App;
